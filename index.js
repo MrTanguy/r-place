e02233702ea8c3abe0c17afdca1a8cc29f77a80e
MrTanguy <tanguy.meignier@gmail.com>
2023-10-12 10:58:33 +0200

    Ajout serveur WS + client (+ connection ws)

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+const CLI_PORT = 3000;
+const WS_PORT = 4000;
+
+const app = express();
+
+
+const wss = new WebSocket.Server({ port: WS_PORT })
+
+wss.on('connection', (ws) => {
+
+    console.log('Client connected');
+
+    ws.send('Welcome to the WebSocket server!');
+
+    ws.on("message", data => {
+    console.log(`Client has sent us: ${data}`)
+    });
+
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(CLI_PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${CLI_PORT}`);
+});
+
